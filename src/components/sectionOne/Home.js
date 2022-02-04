9e436320bacd375152a2269a4170547b121c1905
meno1011 <39692905+meno1011@users.noreply.github.com>
2022-02-04 18:05:21 +0900

    날씨 불러옴

--- src/components/sectionOne/Home.js
+++ src/components/sectionOne/Home.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import logo from "./logo.svg";
 
 export function Home() {
@@ -71,6 +71,7 @@ export function Home() {
         >
           Learn React
         </a>
+        <Outlet />
       </header>
     </>
   );
--- src/components/sectionTwo/Main.js
+++ src/components/sectionTwo/Main.js
@@ -1,39 +1,34 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import styles from "./css/styles.module.css";
 
 export function Main() {
   const [location, setLocation] = useState({ lat: 0, lon: 0 });
-
+  const [data, setData] = useState();
   const getGeolocation = () => {
     if ("geolocation" in navigator) {
       /* 위치정보 사용 가능 */
       console.log(navigator);
-      navigator.geolocation.getCurrentPosition((position) => {
-        console.log("getgeo");
-         setLocation({
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        setLocation({
           lat: position.coords.latitude,
           lon: position.coords.longitude,
         });
+        await axios
+          .get(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${process.env.REACT_APP_WEATHER_API}`
+          )
+          .then((res) => {
+            setData(res.data);
+          });
       });
     } else {
       return alert("위치정보 사용 불가능");
     }
   };
 
-  const weather = async () => {
-    console.log(location);
-    await axios
-      .get(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&appid=${process.env.REACT_APP_WEATHER_API}`
-      )
-      .then((res) => console.log(res.data));
-  };
-
   useEffect(() => {
     getGeolocation();
-    weather();
   }, []);
 
   return (
@@ -42,6 +37,17 @@ export function Main() {
       <div>
         <p>위도 : {location.lat}</p>
         <p>경도 : {location.lon}</p>
+
+        {data && data ? (
+          <>
+            <p>위치 : {data.name}</p>
+            <p>체감온도 : {data.main.feels_like - 273.15}℃</p>
+            <p>현재기온 : {data.main.temp - 273.15}℃</p>
+            <p>습도 : {data.main.humidity}%</p>
+          </>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );

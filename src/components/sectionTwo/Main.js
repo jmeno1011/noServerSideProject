3ddd419e70243eba6da7a9f85279ec72b8de9495
jmeno1011 <jwhltn8282@gmail.com>
2022-02-06 00:09:16 +0900

    add main dom

--- src/components/sectionTwo/Main.js
+++ src/components/sectionTwo/Main.js
@@ -1,54 +1,35 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link, Outlet } from "react-router-dom";
 import styles from "./css/styles.module.css";
 
 export function Main() {
-  const [location, setLocation] = useState({ lat: 0, lon: 0 });
-  const [data, setData] = useState();
-  const getGeolocation = () => {
-    if ("geolocation" in navigator) {
-      /* 위치정보 사용 가능 */
-      console.log(navigator);
-      navigator.geolocation.getCurrentPosition(async (position) => {
-        setLocation({
-          lat: position.coords.latitude,
-          lon: position.coords.longitude,
-        });
-        await axios
-          .get(
-            `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${process.env.REACT_APP_WEATHER_API}`
-          )
-          .then((res) => {
-            setData(res.data);
-          });
-      });
-    } else {
-      return alert("위치정보 사용 불가능");
-    }
-  };
-
-  useEffect(() => {
-    getGeolocation();
-  }, []);
-
   return (
-    <div className={styles.app}>
-      <p>main</p>
-      <div>
-        <p>위도 : {location.lat}</p>
-        <p>경도 : {location.lon}</p>
-
-        {data && data ? (
-          <>
-            <p>위치 : {data.name}</p>
-            <p>체감온도 : {data.main.feels_like - 273.15}℃</p>
-            <p>현재기온 : {data.main.temp - 273.15}℃</p>
-            <p>습도 : {data.main.humidity}%</p>
-          </>
-        ) : (
-          ""
-        )}
+    <>
+      <div className={styles.header}>
+        <div>
+          <Link to="/main">
+            <h2>main</h2>
+          </Link>
+          <Link to="/">
+            <h2>home</h2>
+          </Link>
+        </div>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="weather">weather</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
-    </div>
+      <Outlet />
+
+      <div className={styles.footer}>
+        <p>footer</p>
+      </div>
+    </>
   );
 }
--- src/components/sectionTwo/Weather.js
+++ src/components/sectionTwo/Weather.js
@@ -1,6 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import styles from "./css/styles.module.css";
 
 export function Weather() {
-  return <div>weather</div>;
-}
+  const [location, setLocation] = useState({ lat: 0, lon: 0 });
+  const [data, setData] = useState();
+  const getGeolocation = () => {
+    if ("geolocation" in navigator) {
+      /* 위치정보 사용 가능 */
+      console.log(navigator);
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        setLocation({
+          lat: position.coords.latitude,
+          lon: position.coords.longitude,
+        });
+        await axios
+          .get(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${process.env.REACT_APP_WEATHER_API}`
+          )
+          .then((res) => {
+            setData(res.data);
+          });
+      });
+    } else {
+      return alert("위치정보 사용 불가능");
+    }
+  };
 
+  useEffect(() => {
+    getGeolocation();
+  }, []);
+  return (
+    <>
+      <div className={styles.app}>
+        <div>
+          <p>위도 : {location.lat}</p>
+          <p>경도 : {location.lon}</p>
+
+          {data && data ? (
+            <>
+              <p>위치 : {data.name}</p>
+              <p>체감온도 : {Math.round(data.main.feels_like - 273.15)}℃</p>
+              <p>현재기온 : {Math.round(data.main.temp - 273.15)}℃</p>
+              <p>습도 : {data.main.humidity}%</p>
+            </>
+          ) : (
+            ""
+          )}
+        </div>
+      </div>
+    </>
+  );
+}

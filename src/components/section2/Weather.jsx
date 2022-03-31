79c2ca8af4176f928be16e1ecb77d772844f7a51
jmeno1011 <jwhltn8282@gmail.com>
2022-03-31 22:44:51 +0900

    /weather styled-component일부 적용 및 screen크기에 따라 weather-box 중앙으로 변경

--- src/components/section2/Weather.jsx
+++ src/components/section2/Weather.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Icon } from "@iconify/react";
+import styled from "styled-components";
 
 export function Weather() {
   const [location, setLocation] = useState({ lat: 0, lon: 0 });
@@ -36,79 +37,70 @@ export function Weather() {
       <div>
         {data && data ? (
           <>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                background: "rgb(109 213 235)",
-                padding: "0.5rem 0.8rem",
-                borderRadius: "1rem",
-                alignItems: "center",
-                width: "320px",
-              }}
-            >
-              <div>{data.name}</div>
-              <div
-                style={{
-                  width: "100%",
-                  display: "flex",
-                  justifyContent: "space-around",
-                }}
-              >
-                <div style={{ width: "150px" }}>
-                  <h1 style={{ margin: 0, marginTop: "1rem" }}>
-                    {Math.round(data.main.temp - 273.15)}℃
-                  </h1>
-                  <p style={{ margin: 0 }}>{data.weather[0].description}</p>
-                  <p
+            <SectionWrap>
+              <PicSection>
+                <div>{data.name}</div>
+                <div
+                  style={{
+                    width: "100%",
+                    display: "flex",
+                    justifyContent: "space-around",
+                  }}
+                >
+                  <div style={{ width: "150px" }}>
+                    <h1 style={{ margin: 0, marginTop: "1rem" }}>
+                      {Math.round(data.main.temp - 273.15)}℃
+                    </h1>
+                    <p style={{ margin: 0 }}>{data.weather[0].description}</p>
+                    <p
+                      style={{
+                        margin: 0,
+                        fontSize: "0.75rem",
+                        letterSpacing: "0.7px",
+                        fontWeight: "700",
+                        color: "rgba(0,0,0,0.7)",
+                      }}
+                    >
+                      humidity: {data.main.humidity}%
+                    </p>
+                  </div>
+                  <div
                     style={{
-                      margin: 0,
-                      fontSize: "0.75rem",
-                      letterSpacing: "0.7px",
-                      fontWeight: "700",
-                      color: "rgba(0,0,0,0.7)",
+                      display: "flex",
+                      alignItems: "center",
                     }}
                   >
-                    humidity: {data.main.humidity}%
-                  </p>
+                    <img
+                      src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+                      alt=""
+                    />
+                  </div>
                 </div>
                 <div
                   style={{
+                    width: "100%",
                     display: "flex",
                     alignItems: "center",
                   }}
                 >
-                  <img
-                    src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
-                    alt=""
-                  />
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      fontSize: "1.5rem",
+                      color: "darkred",
+                    }}
+                  >
+                    <Icon icon="el:map-marker" />
+                  </div>
+                  {/* <div> */}
+                  <p style={{ margin: "0.5rem" }}>lat : {location.lat}</p>/
+                  <p style={{ margin: "0.5rem" }}>lon : {location.lon}</p>
+                  {/* </div> */}
                 </div>
-              </div>
-              <div
-                style={{
-                  width: "100%",
-                  display: "flex",
-                  alignItems: "center",
-                }}
-              >
-                <div
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    fontSize: "1.5rem",
-                    color: "darkred",
-                  }}
-                >
-                  <Icon icon="el:map-marker" />
-                </div>
-                {/* <div> */}
-                <p style={{ margin: "0.5rem" }}>lat : {location.lat}</p>/
-                <p style={{ margin: "0.5rem" }}>lon : {location.lon}</p>
-                {/* </div> */}
-              </div>
-            </div>
+              </PicSection>
+            </SectionWrap>
             <p>체감온도 : {Math.round(data.main.feels_like - 273.15)}℃ </p>
-            <p>습도 : {data.main.humidity}%</p>
             <p>최저 온도: {Math.round(data.main.temp_min - 273.15)}℃</p>
             <p>최저 온도: {Math.round(data.main.temp_max - 273.15)}℃</p>
           </>
@@ -119,4 +111,19 @@ export function Weather() {
     </>
   );
 }
-// new_shap_values = [ 0.040970993041992185, 0.04296798706054687, 0.010036341349283856, 0.013927332560221356, 0.07180010477701823 ]
+
+const PicSection = styled.section`
+  width: 320px;
+  padding: 0.5rem 0.8rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: rgb(109, 213, 235);
+`;
+
+const SectionWrap = styled.div`
+  display: flex;
+  @media only screen and (max-width: 768px) {
+    justify-content: center;
+  }
+`;

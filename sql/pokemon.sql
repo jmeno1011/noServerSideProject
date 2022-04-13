2beb20d62b2200cc6ee8fad623340726a9d113c6
jmeno1011 <jwhltn8282@gmail.com>
2022-04-14 00:37:18 +0900

    add insert sql

--- sql/pokemon.sql
+++ sql/pokemon.sql
@@ -13,3 +13,5 @@ CREATE TABLE pokemon
     url text,
     primary key(no)
 );
+
+INSERT INTO `maintest`.`pokemon` (`no`, `name`, `type`, `url`) VALUES ('001', '이상해씨', '풀,독', 'https://meno-image-storage.s3.ap-northeast-2.amazonaws.com/noServerSideProject/%ED%8F%AC%EC%BC%93%EB%AA%AC/001%EC%9D%B4%EC%83%81%ED%95%B4%EC%94%A8.png');

ac4acfbf38137cbfff16c65b1749ae3abdeb61b7
meno1011 <39692905+meno1011@users.noreply.github.com>
2022-04-13 18:03:46 +0900

    quizprop warning지움

--- src/components/section2/QuizProp.js
+++ src/components/section2/QuizProp.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styledComponents from "styled-components";
 

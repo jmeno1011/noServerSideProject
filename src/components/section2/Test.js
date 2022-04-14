ece93e544a4440cddab7bc65bad174730a3bf634
jmeno1011 <jwhltn8282@gmail.com>
2022-04-15 01:03:47 +0900

    포켓몬 5개까지가 적당 width:800px일때

--- src/components/section2/Test.js
+++ src/components/section2/Test.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
 const Test = () => {
-  const [cheked, setCheked] = useState(false);
   let chk = document.getElementById("check");
   const onClickHandle = () => {
     console.log(chk);
@@ -30,8 +30,105 @@ const Test = () => {
       </p>
       <p>안녕하세요 Say Hello</p>
       <p>안녕하세요 Say Hello</p>
+      <div style={{ display: "flex", maxWidth: "800px" }}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            padding: "0 1rem",
+          }}
+        >
+          <Pockemon src="https://meno-image-storage.s3.ap-northeast-2.amazonaws.com/noServerSideProject/%ED%8F%AC%EC%BC%93%EB%AA%AC/001%EC%9D%B4%EC%83%81%ED%95%B4%EC%94%A8.png" />
+          <div style={{ padding: "1rem" }}>
+            <h2>001</h2>
+            <p>
+              이름 : {"이상해씨"}
+              <br />
+              타입 : {"풀, 독"}
+            </p>
+          </div>
+          <div></div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            padding: "0 1rem",
+          }}
+        >
+          <Pockemon src="https://meno-image-storage.s3.ap-northeast-2.amazonaws.com/noServerSideProject/%ED%8F%AC%EC%BC%93%EB%AA%AC/001%EC%9D%B4%EC%83%81%ED%95%B4%EC%94%A8.png" />
+          <div style={{ padding: "1rem" }}>
+            <h2>001</h2>
+            <p>
+              이름 : {"이상해씨"}
+              <br />
+              타입 : {"풀, 독"}
+            </p>
+          </div>
+          <div></div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            padding: "0 1rem",
+          }}
+        >
+          <Pockemon src="https://meno-image-storage.s3.ap-northeast-2.amazonaws.com/noServerSideProject/%ED%8F%AC%EC%BC%93%EB%AA%AC/001%EC%9D%B4%EC%83%81%ED%95%B4%EC%94%A8.png" />
+          <div style={{ padding: "1rem" }}>
+            <h2>001</h2>
+            <p>
+              이름 : {"이상해씨"}
+              <br />
+              타입 : {"풀, 독"}
+            </p>
+          </div>
+          <div></div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            padding: "0 1rem",
+          }}
+        >
+          <Pockemon src="https://meno-image-storage.s3.ap-northeast-2.amazonaws.com/noServerSideProject/%ED%8F%AC%EC%BC%93%EB%AA%AC/001%EC%9D%B4%EC%83%81%ED%95%B4%EC%94%A8.png" />
+          <div style={{ padding: "1rem" }}>
+            <h2>001</h2>
+            <p>
+              이름 : {"이상해씨"}
+              <br />
+              타입 : {"풀, 독"}
+            </p>
+          </div>
+          <div></div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            padding: "0 1rem",
+          }}
+        >
+          <Pockemon src="https://meno-image-storage.s3.ap-northeast-2.amazonaws.com/noServerSideProject/%ED%8F%AC%EC%BC%93%EB%AA%AC/001%EC%9D%B4%EC%83%81%ED%95%B4%EC%94%A8.png" />
+          <div style={{ padding: "1rem" }}>
+            <h2>001</h2>
+            <p>
+              이름 : {"이상해씨"}
+              <br />
+              타입 : {"풀, 독"}
+            </p>
+          </div>
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Test;
+
+const Pockemon = styled.img`
+  width: 100%;
+  object-fit: cover;
+`;

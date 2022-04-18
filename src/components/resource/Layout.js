f7582dcb92a064f613ea54b13f274d380e17559b
jmeno1011 <jwhltn8282@gmail.com>
2022-04-18 22:21:06 +0900

    포켓몬 모양 확인

--- src/components/resource/Layout.js
+++ src/components/resource/Layout.js
@@ -1,15 +1,22 @@
 import React from "react";
 import PockemonCard from "./components/PockemonCard";
 import SearchBar from "./components/SearchBar";
+import styled from "styled-components";
 
 const Layout = () => {
   return (
     <div style={{ backgroundColor: "white", height: "100vh" }}>
       <SearchBar />
-      <PockemonCard />
-      Layout
+      <CardSpace>
+        <PockemonCard />
+      </CardSpace>
     </div>
   );
 };
 
 export default Layout;
+
+const CardSpace = styled.div`
+  padding: 1rem;
+  background-color: darkred;
+`;
--- src/components/resource/components/PockemonCard.js
+++ src/components/resource/components/PockemonCard.js
@@ -15,31 +15,68 @@ const PockemonCard = () => {
     fetchUser();
   }, []);
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: "0 1rem",
-      }}
-    >
+    <CardWrapper>
       <Pockemon src="https://meno-image-storage.s3.ap-northeast-2.amazonaws.com/noServerSideProject/%ED%8F%AC%EC%BC%93%EB%AA%AC/001%EC%9D%B4%EC%83%81%ED%95%B4%EC%94%A8.png" />
-      <div style={{ padding: "1rem" }}>
-        <h2>001</h2>
-        <p>
-          이름 : {"이상해씨"}
-          <br />
-          타입 : {"풀, 독"}
-        </p>
-      </div>
-      <div></div>
-    </div>
+      <CardInfo>
+        <h3>
+          <p>001</p>
+          이상해씨
+        </h3>
+        <div>
+          <GrassSpan>풀</GrassSpan>
+          <Poison>독</Poison>
+        </div>
+      </CardInfo>
+    </CardWrapper>
   );
 };
 
 export default PockemonCard;
 
 const Pockemon = styled.img`
-  /* height: 150px; */
-  width: 150px;
+  width: 100px;
   object-fit: cover;
+`;
+
+const CardWrapper = styled.div`
+  width: 150px;
+  padding: 0.5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: white;
+  border-radius: 40px;
+  h3 {
+    margin: 0;
+    font-size: 1.1rem;
+    margin-bottom: 4px;
+  }
+  p {
+    margin: 0;
+    font-size: 1rem;
+  }
+  label {
+    margin: 0;
+  }
+  span {
+    margin: 0;
+  }
+`;
+
+const CardInfo = styled.div`
+  padding: 0.5rem;
+  width: 100px;
+`;
+
+const GrassSpan = styled.span`
+  background-color: #7ab766;
+  color: white;
+`;
+const Poison = styled.span`
+  background-color: #a16ec2;
+  color: white;
+`;
+
+const TypeSpan = styled.span`
+  width: 100%;
 `;

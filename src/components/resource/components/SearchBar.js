9fd0178581b0b22471e47b2da209842edfd8fe8e
jmeno1011 <jwhltn8282@gmail.com>
2022-04-20 23:30:42 +0900

    input state 추가

--- src/components/resource/components/SearchBar.js
+++ src/components/resource/components/SearchBar.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import monsterBall from "../css/img/icon_ball_b.png";
 import { Icon } from "@iconify/react";
 
 const SearchBar = () => {
+  const [keyword, setKeyword] = useState("");
+  const search = (e) => {
+    e.preventDefault();
+    console.log("keyword::", keyword);
+  };
   return (
     <InputWrapper>
       <SearchBarTitle>
         <img src={monsterBall} />
         포켓몬 도감
       </SearchBarTitle>
-      <form style={{ display: "flex", alignItems: "center" }}>
-        <Input type={"text"} placeholder="search your pockemon!" />
+      <form style={{ display: "flex", alignItems: "center" }} onSubmit={search}>
+        <Input
+          type={"text"}
+          placeholder="search your pockemon!"
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+        />
         <SearchButton>
           <Icon icon="ant-design:search-outlined" />
         </SearchButton>

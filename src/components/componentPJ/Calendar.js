0c3ed1f84d8e003a396818229613c3e2c39658cf
jmeno1011 <jwhltn8282@gmail.com>
2022-06-08 00:50:33 +0900

    이번달의 첫번째 요일과 마지막 요일부터 알고 실마리 잡기

--- src/components/componentPJ/Calendar.js
+++ src/components/componentPJ/Calendar.js
@@ -3,13 +3,39 @@ import styled from "styled-components";
 
 const Calendar = () => {
   const [now, setNow] = useState(new Date());
+  const cYear = now.getFullYear();
+  const cMonth = now.getMonth();
+  const pDate = new Date(cYear, cMonth);
+  const nDate = new Date(cYear, cMonth + 1);
   console.log("now:", new Date());
+
+  const getDay = (n) => {
+    switch (n % 7) {
+      case 0:
+        return "일";
+      case 1:
+        return "월";
+      case 2:
+        return "화";
+      case 3:
+        return "수";
+      case 4:
+        return "목";
+      case 5:
+        return "금";
+      case 6:
+        return "토";
+    }
+  };
+  console.log("getDay:", getDay(2));
   return (
     <div>
       Calendar
-      <h1>
-        현재 날짜 : {now.getFullYear()}년 {now.getMonth()}월 {now.getDate()}일{" "}
-      </h1>
+      <h2>이번달의 첫째 날짜 : {pDate.toString()} </h2>
+      <h2>1일 요일 날짜 : {getDay(pDate.getDay())}</h2>
+      <h2>현재 날짜 : {now.toString()}</h2>
+      {/* // Sunday - Saturday : 0 - 6 */}
+      <h2>다음 달 : {nDate.toString()}</h2>
       <Grid>
         <div>
           <div>col1</div>
